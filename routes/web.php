fe851460b1c9e218b5438144d0ebaf4194beaa22
proshehab <shehab.shakib@gmail.com>
2026-01-12 20:23:24 +0600

    update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\FrontendController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', [FrontendController::class, 'index'])->name('frontend.master');
