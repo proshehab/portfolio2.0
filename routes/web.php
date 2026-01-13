cf72d7e7ae470e756b0ed896cf75af877d9c9f61
proshehab <shehab.shakib@gmail.com>
2026-01-13 13:13:51 +0600

    feat: add admin login view and update routes for contact message

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AuthController;
 use App\Http\Controllers\FrontendController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,4 +10,8 @@ Route::get('/about', [FrontendController::class, 'about'])->name('frontend.about
 Route::get('/service', [FrontendController::class, 'service'])->name('frontend.service');
 Route::get('/portfolio', [FrontendController::class, 'portfolio'])->name('frontend.portfolio');
 Route::get('/blog', [FrontendController::class, 'blog'])->name('frontend.blog');
+Route::get('/blog/details/{id}', [FrontendController::class, 'blogDetails'])->name('frontend.blog.details');
 Route::get('/contact', [FrontendController::class, 'contact'])->name('frontend.contact');
+Route::post('/contact/message', [FrontendController::class, 'contactMessage'])->name('frontend.contact.message');
+
+Route::get('admin/login', [AuthController::class, 'loginPage'])->name('admin.login.page');
