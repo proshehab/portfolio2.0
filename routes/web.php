970a7092309adaeaa3b54d66ae8ffd9a67bc4465
proshehab <shehab.shakib@gmail.com>
2026-01-13 10:46:42 +0600

    Refactor web routes to use FrontendController for the home page

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,6 @@
 use App\Http\Controllers\FrontendController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/', [FrontendController::class, 'index'])->name('frontend.master');
 Route::get('/about', [FrontendController::class, 'about'])->name('frontend.about');
