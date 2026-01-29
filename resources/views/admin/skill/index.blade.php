ccc54454c037d9d22f1c25d66f02d7163acbba5a
proshehab <shehab.shakib@gmail.com>
2026-01-29 10:51:09 +0600

    feat: add skill form with input fields and layout enhancements

--- resources/views/admin/skill/index.blade.php
+++ resources/views/admin/skill/index.blade.php
@@ -1,4 +1,94 @@
 @extends('admin.layouts.master')
 @section('content')
 
+<div class="container-xxl">
+
+    <div class="py-3 d-flex align-items-sm-center flex-sm-row flex-column">
+        <div class="flex-grow-1">
+            <h4 class="fs-18 fw-semibold m-0">Skill</h4>
+        </div>
+
+        <div class="text-end">
+            <ol class="breadcrumb m-0 py-0">
+                <li class="breadcrumb-item"><a href="javascript: void(0);">Forms</a></li>
+                <li class="breadcrumb-item active">Skill</li>
+            </ol>
+        </div>
+    </div>
+
+    <!-- General Form -->
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+
+                <div class="card-header">
+                    <h5 class="card-title mb-0">Input Type</h5>
+                </div><!-- end card header -->
+
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <form>
+                                <div class="mb-3">
+                                    <label for="simpleinput" class="form-label">Text</label>
+                                    <input type="text" id="simpleinput" class="form-control">
+                                </div>
+
+                                <div class="mb-3">
+                                    <label for="simpleinput" class="form-label">Text</label>
+                                    <input type="text" id="simpleinput" class="form-control">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="simpleinput" class="form-label">Text</label>
+                                    <input type="text" id="simpleinput" class="form-control">
+                                </div>
+
+                                <div class="mb-3">
+                                    <label for="simpleinput" class="form-label">Text</label>
+                                    <input type="text" id="simpleinput" class="form-control">
+                                </div>
+
+
+                                <div class="col-auto">
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+
+                            </form>
+                        </div>
+
+                        <div class="col-lg-6">
+
+
+                            <div class="mb-3">
+                                <label for="simpleinput" class="form-label">Text</label>
+                                <input type="text" id="simpleinput" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label for="simpleinput" class="form-label">Text</label>
+                                <input type="text" id="simpleinput" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label for="simpleinput" class="form-label">Text</label>
+                                <input type="text" id="simpleinput" class="form-control">
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="simpleinput" class="form-label">Text</label>
+                                <input type="text" id="simpleinput" class="form-control">
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+</div> <!-- container-fluid -->
 @endsection
